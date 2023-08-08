fd64bfdb12717854e8b8491b06e57cb7b17b0f9c
choifi <chlals9930@gmail.com>
2023-08-08 12:23:44 +0900

    product registration

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -6,11 +6,11 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import com.shop.dto.ItemFormDto;
 
 @Entity
 @Table(name="item")
-@Getter
-@Setter
+@Getter @Setter
 @ToString
 public class Item extends BaseEntity {
 
@@ -34,5 +34,13 @@ public class Item extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
 }
